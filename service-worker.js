a2177cce6041ff7087812bbaa517e40b791b4931
alainreichling <alain.reichling@me.com>
2025-10-04 08:33:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+
+// Offline cache for Margeur Pro v6.2
+const CACHE_NAME = 'margeurpro-v6-2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
